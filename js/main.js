216b60e2e0efec599013df95e19ffb6b71440e96
FraCuboni <fracuboni@gmail.com>
2024-06-27 15:23:16 +0200

    feat one email extracted

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ createApp({
     data() {
         return {
             // data
+            email : '',
 
 
         };
@@ -15,6 +16,18 @@ createApp({
     },
     mounted(){
         // mounted
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((axiosEmail)=>{
+            const result = axiosEmail.data.response;
+            console.log(result);
+        })
         
     }
 }).mount('#app');
+
+
+
+// Descrizione:
+// Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
